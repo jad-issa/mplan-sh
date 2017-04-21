f322e694c3b119552419a327d512e50062fb4594
Jad Issa <foss-the-world@protonmail.com>
2017-04-21 10:24:19 +0300

    Added UI sugar
    Added some colors, a few nice responses, etc...
    There is nothing new in the functioning, but now
    it's much clearer to use.

--- mplan.sh
+++ mplan.sh
@@ -1,19 +1,28 @@
 #!/bin/sh
 
+# Note: ID is the filename
+
 dir=$HOME/.mplan
 
 case "$1" in
     "show")
 	for f in $(ls $dir)
 	do
-	    echo; echo $f
-	    cat $dir/$f
+	    echo "[0;31m----------------[0m"     # UI sugar
+	    echo "[0;32mID:$f[0m"		     # UI sugar
+	    cat $dir/$f				     # UI sugar
 	done
+	echo "[0;31m----------------[0m" 	     # UI sugar
 	;;
     "create")
-	echo $2 > $dir/$RANDOM
+	ID=$RANDOM
+	echo $2 > $dir/$ID
+	echo "[0;32mID:$ID[0m"		     # UI sugar
+	echo $2
 	;;
     "remove")
+	echo -e "[0;31mX[0m\t[0;32mID:$2[0m" # UI sugar
+	echo -e "\t`cat $dir/$2`"		     # UI sugar
 	[ "$2" = '*' ] && rm $dir/* || rm $dir/$2
 	;;
     *)
