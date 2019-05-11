2eead9b5706d8f1862645803495ecdfcfacc5ebe
Jad Issa <25721205+jad-issa@users.noreply.github.com>
2019-05-11 14:52:02 +0300

    Changed command names and better commenting
    Changed command names to more closely resemble common Linux commands
    (ls, rm) and to shorten their size for typing. Also, commented a little
    better than a simple "UI Sugar".

--- mplan.sh
+++ mplan.sh
@@ -10,32 +10,37 @@ if [ ! -d "$dir" ]; then
 fi
 
 case "$1" in
-    "show")
+    "ls")
 	for f in $(ls $dir)
 	do
-	    echo "[0;31m----------------[0m"     # UI sugar
-	    echo "[0;32mID:$f[0m"		     # UI sugar
-	    cat $dir/$f				     # UI sugar
+	    echo "[0;31m----------------[0m" # Red line before every note
+	    echo "[0;32mID: $f[0m"	         # Note ID in blue
+	    cat $dir/$f                          # Show the actual content
 	done
-	echo "[0;31m----------------[0m" 	     # UI sugar
+	echo "[0;31m----------------[0m"     # Single red line at the end
 	;;
-    "create")
+
+    "add")
 	ID=$RANDOM
 	echo $2 > $dir/$ID
-	echo "[0;32mID:$ID[0m"		     # UI sugar
+	echo "[0;32mID: $ID[0m"	             # ID in green
 	echo $2
 	;;
-    "remove")
-	echo -e "[0;31mX[0m\t[0;32mID:$2[0m" # UI sugar
-	echo -e "\t`cat $dir/$2`"		     # UI sugar
+
+    "rm")
+	echo -e "[0;31mX[0m\t[0;32mID: $2[0m" # Red X and ID in green
+	echo -e "\t`cat $dir/$2`"		              # Content of the note
 	[ "$2" = '*' ] && rm $dir/* || rm $dir/$2
 	;;
+
     *)
 	echo
+    echo 'Note: only one-line notes are supported'
+    echo 
 	echo 'Usage:'
-	echo '      mplan show               show current tasks'
-	echo '      mplan create content     create a new task'
-	echo '      mplan remove id          remove task by id (use \* for all)'
+	echo '      mplan ls                 list current tasks'
+	echo '      mplan add content        create a new task'
+	echo '      mplan rm id              remove task by id (use \* for all)'
 	echo '      mplan help               print this help'
 	echo
 esac
