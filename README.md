33e16300af82630ded94d199b0fd9fd75a445603
Jad Issa <25721205+jad-issa@users.noreply.github.com>
2019-05-11 15:54:57 +0300

    Added support for multiline notes.

--- README.md
+++ README.md
@@ -2,11 +2,18 @@
 ## install
 - clone repository and save it somewhere safe
 - create ~/.mplan
-- alias the script for ease of use
+- alias the script for ease of use (I find that using 'tod' (for todo) can ease things quite a bit
+  on QWERTY keyboards.)
+
+## usage
+Use `mplan.sh help` to get usage information. 
 
 ## about
 This is the successor of [splan], and is not just simple, but microscopically small.
-It is made for simple one-line tasks/notes, so don't abuse it!
+mplan supports multi-line notes from standard input with `mplan.sh add`.
+
+The software is not stable, and is kept as such so as to be *very* simple. Please consider using it
+carefully.
 
 ## license
 This software is licensed under the GPL-3.0 license included in the github repository.
--- mplan.sh
+++ mplan.sh
@@ -1,18 +1,12 @@
 #!/bin/sh
 
-# Note: ID is the filename
-
-dir=$HOME/.mplan
-
 # Create the directory if it doesn't exist already
-if [ ! -d "$dir" ]; then
-    mkdir -p "$dir"
-fi
+dir=$HOME/.mplan
+[ ! -d "$dir" ] && mkdir -p "$dir"
 
 case "$1" in
     "ls")
-	for f in $(ls $dir)
-	do
+	for f in $(ls $dir); do
 	    echo "[0;31m----------------[0m" # Red line before every note
 	    echo "[0;32mID: $f[0m"	         # Note ID in blue
 	    cat $dir/$f                          # Show the actual content
@@ -21,26 +15,36 @@ case "$1" in
 	;;
 
     "add")
-	ID=$RANDOM
-	echo $2 > $dir/$ID
-	echo "[0;32mID: $ID[0m"	             # ID in green
-	echo $2
+	ID=$RANDOM # It's not worth complicating things to safely verify no overlap.
+    if [ -z "$2" ]; then
+        cat > $dir/$ID
+    else
+        echo $2 > $dir/$ID
+    fi
+    echo "[0;32mID: $ID[0m"	             # ID in green
+	cat $dir/$ID
 	;;
 
     "rm")
 	echo -e "[0;31mX[0m\t[0;32mID: $2[0m" # Red X and ID in green
-	echo -e "\t`cat $dir/$2`"		              # Content of the note
-	[ "$2" = '*' ] && rm $dir/* || rm $dir/$2
+	if [ "$2" = "all" ]; then
+        rm $dir/*
+    else
+        cat $dir/$2
+        rm $dir/$2
+    fi
 	;;
 
     *)
-	echo
-    echo 'Note: only one-line notes are supported'
     echo 
 	echo 'Usage:'
 	echo '      mplan ls                 list current tasks'
 	echo '      mplan add content        create a new task'
-	echo '      mplan rm id              remove task by id (use \* for all)'
+    echo '            NOTE:   if you omit `content`,'
+    echo '                    then standard input'
+    echo '                    is considered (using cat)'
+    echo
+	echo '      mplan rm id              remove task by id (possibly: `rm all`)'
 	echo '      mplan help               print this help'
 	echo
 esac
